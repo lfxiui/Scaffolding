32670fe74c4d4c02d631cff0515f37b97734a601
lfxiui <806941929@qq.com>
2018-02-24 13:20:16 +0800

    Created by IDEA

---
+++ README.md
@@ -0,0 +1,10 @@
+#My First Spring Boot Scaffolding
+##Info
+- **Name**:Scaffolding
+- **Create Time**:2018年2月24日13点11分
+##Tools
+- **Activiti**
+- **Mybatis**
+- **Thymeleaf**
+- **JDBC**
+- **MySQL**
---
+++ src/main/java/com/lfxiui/scaffolding/ScaffoldingApplication.java
@@ -0,0 +1,12 @@
+package com.lfxiui.scaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffoldingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaffoldingApplication.class, args);
+	}
+}
